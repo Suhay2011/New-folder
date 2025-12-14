6fac5bb88c1ff43c7f7e6e2150290c633efaf416
Suhay2011 <suhayl.abrahams3@gmail.com>
2025-12-14 18:23:18 +0200

    first commit

---
+++ AddNumbers.java
@@ -0,0 +1,11 @@
+public class AddNumbers {
+    Run|Debug
+    public static void main(string[] args ){
+        int numl = 10; // First number
+        int num2 = 20; //Second number
+
+        int sum = num1 + num2; // Add the numbers
+
+        System.outprintln("Sum is:" +sum);
+    }
+}
---
+++ hello.java
@@ -0,0 +1,6 @@
+public class hello{
+    Run|Debug
+    public static void main (String[] args) {
+        system.out.println(x:"Hi this is my first program in java...")
+    }
+}
